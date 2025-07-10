ada2b4527b8412fbf8576f210c3cb8a052c02eb9
Preeti Mallick <preetimallick2001@gmail.com>
2025-07-10 22:47:40 +0530

    design_changes

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+# we are learning django session base authentication system
 # Create your views here.
 def login_view(request):
     return render(request,'login.html')
 
 def register_view(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        print(firstname,lastname,email,username)
+        u=User.objects.create(
+            first_name = firstname,
+            last_name = lastname,
+            email = email,
+            username = username,
+        )
+        # set_password() used to store password in encrypted format
+        u.set_password(password)
+        u.save()
+        return redirect('login')
+    #IntegrityError - unique constraint failed:auth_username -  when same username is used
+        
     return render(request,'register.html')
