263de83f139546292ebb7adb25587a50118839e1
Preeti Mallick <preetimallick2001@gmail.com>
2025-07-10 19:46:39 +0530

    login_and_register

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from base import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('home/',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('bookings/',views.bookings,name='bookings'),
     path('history/',views.history,name='history'),
