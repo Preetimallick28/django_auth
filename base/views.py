b05ece7913aa186f5ff9fb2e31633252d635f423
Preeti Mallick <preetimallick2001@gmail.com>
2025-07-12 20:50:34 +0530

    authorization

--- base/views.py
+++ base/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+#@login_required - this is a decorator which will not allowed user to access with login or user verification
+@login_required(login_url='login')
 def home(request):
     return render(request,'home.html')
+@login_required
 def about(request):
     return render(request,'about.html')
+@login_required
 def bookings(request):
     return render(request,'bookings.html')
+@login_required
 def history(request):
     return render(request,'history.html')
 
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate ,login , logout
-from django.contrib import login_required
+from django.contrib.auth.decorators import login_required
 # from django.contrib import messages
 # we are learning django session base authentication system
 # Create your views here.
