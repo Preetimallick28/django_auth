f09982a7291fc815f7175fb6d21cea6aa3ef4292
Preeti Mallick <preetimallick2001@gmail.com>
2025-07-12 21:58:08 +0530

    views changes

--- base/views.py
+++ base/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-#@login_required - this is a decorator which will not allowed user to access with login or user verification
+# @login_required - this is a decorator which will not allowed user to access with login or user verification
+
+# assignement
+# create new flight and task with authentication system
+# old password and new password
+# handle integrity error
+
+
+
+
 @login_required(login_url='login')
 def home(request):
-    return render(request,'home.html')
+    if request.user.is_authenticated: #it will return true and if false template will not rendered
+        return render(request,'home.html')
+
 @login_required
 def about(request):
-    return render(request,'about.html')
+    if request.user.is_authenticated:
+        return render(request,'about.html')
+
 @login_required
 def bookings(request):
-    return render(request,'bookings.html')
+    if request.user.is_authenticated:
+        return render(request,'bookings.html')
+
 @login_required
 def history(request):
-    return render(request,'history.html')
+    if request.user.is_authenticated:
+        return render(request,'history.html')
 
-# sorry sir
